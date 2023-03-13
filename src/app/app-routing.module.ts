8740dd7f67ca9fde69aaa1fab4de993f984b4565
Dennis Ammen <dennis.ammen@outlook.com>
2023-03-13 19:00:10 +0100

    Reset project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { GameComponent } from './game/game.component';
-import { StartScreenComponent } from './start-screen/start-screen.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: '', component: StartScreenComponent },
-  { path: 'game', component: GameComponent }
+  { path: '', component: AppComponent }
 ];
 
 @NgModule({
