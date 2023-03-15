0b72b3c9abe2abdce56449173e6a892ff31272dd
Dennis Ammen <dennis.ammen@outlook.com>
2023-03-15 22:09:47 +0100

    Import new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartScreenComponent } from './start-screen/start-screen.component';
+import { GameScreenComponent } from './game-screen/game-screen.component';
+import { PlayerDisplayComponent } from './player-display/player-display.component';
+import { PlayerComponent } from './player/player.component';
+import { InstructionsComponent } from './instructions/instructions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StartScreenComponent
+    StartScreenComponent,
+    GameScreenComponent,
+    PlayerDisplayComponent,
+    PlayerComponent,
+    InstructionsComponent
   ],
   imports: [
     BrowserModule,
